816e5b1678fb3cb7bed192c16accaaf4eb0b20c4
Arika Turing <ablenker@gmail.com>
2021-01-16 15:52:57 -0700

    Start initial setup and Enigma first test

---
+++ test/enigma_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+require 'date'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma 
+  end
+
+end
